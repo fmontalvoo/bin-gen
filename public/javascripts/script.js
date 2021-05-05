4d6842d0d98f0c15b6e89e65975378e9e8ddde08
Frank Montalvo Ochoa <fmontalvoo@ups.edu.ec>
2021-05-05 18:08:55 -0500

    Complete first version.
    Signed-off-by: Frank Montalvo Ochoa <fmontalvoo@ups.edu.ec>

--- public/javascripts/script.js
+++ public/javascripts/script.js
@@ -7,38 +7,31 @@ $('#number').on('keypress change', function () {
 
 // Escucha el evento click del boton para generar las tarjetas.
 $("button").on("click", function () {
-    let number = $("#number").val().trim().replace(' ', ''); // Recupera el valor del input.
-    let valid = $('#valid').val().trim();
+    let number = $("#number").val().trim().replace(/ /g, ""); // Recupera el valor del input.
+    let valid = $('#valid').val().trim().replace('/', '|');
     let cvv = $('#cvv').val().trim();
     let range = $('#range').val();
-    range = range > 100 ? 100 : range < 1 ? 1 : range;
+    range = range > 9999 ? 9999 : range < 1 ? 1 : range;
 
-    let randomV = $('#randomV').is(':checked');
-    let randomC = $('#randomC').is(':checked');
+    let randomG = $('#randomG').is(':checked');
 
-    console.log(randomV, randomC);
     $.ajax({
         type: 'GET',
         url: '/generate',
         data: {
             number,
             valid,
-            randomV,
             cvv,
-            randomC,
-            range
+            range,
+            randomG
         },
         contentType: 'application/json; charset=utf-8',
         success: function (data) {
             let result = '';
             for (var i = 0; i < data.length; i++)
-                result += formatItem(data[i]);
+                result += data[i] + '\n';
             $('#result').val(result);
         }
     });
 });
 
-
-function formatItem(item) {
-    return item.number + '|' + item.valid + '|' + item.cvv + '\n';
-}
--- routes/index.js
+++ routes/index.js
@@ -1,6 +1,8 @@
 var express = require('express');
 var router = express.Router();
 
+const { generator } = require('../src/generator');
+
 /* GET home page. */
 router.get('/', function (req, res, next) {
   res.render('index', { title: 'BIN-GENERATOR' });
@@ -8,8 +10,8 @@ router.get('/', function (req, res, next) {
 
 router.get('/generate', function (req, res, next) {
   const params = req.query;
-  const { number, valid, randomV, cvv, randomC, range } = params;
-  res.send([{ number, valid, cvv }, { number, valid, cvv }]);
+  const { number, valid, cvv, range, randomG } = params;
+  res.send(generator({ number: number, valid: valid, cvv: cvv, range: range, randomG: randomG }));
 });
 
 module.exports = router;
